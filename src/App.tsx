c020a68eae03094fa4e8bc347abfe03140c180d1
Anton Serous <aserous@th2.com>
2020-06-04 21:21:36 +1200

    Use type literals in reducer and move horses responsibilities to List

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import React, { FC, useEffect, useReducer } from "react";
+import React, { FC, useReducer } from "react";
 
 import { HorseList } from "./HorseList";
 
-import { Horse, State, Reducer, ACTION_TYPE } from "./types";
+import { Horse, State, Reducer } from "./types";
 
 import "./App.css";
 
 const initialState: State = {
   status: "idling",
   horses: [],
-  page: 'horse-list', 
+  page: "horse-list",
 };
 
 const reducer: Reducer = (state, action) => {
   switch (action.type) {
-    case ACTION_TYPE.FETCH_HORSES:
+    case "FETCH_HORSES":
       return {
         ...state,
         status: "loading",
       };
-    case ACTION_TYPE.FETCH_HORSES_SUCCESS:
+    case "FETCH_HORSES_SUCCESS":
       return {
         ...state,
         status: "loadingSuccess",
         horses: action.payload || state.horses,
       };
-    case ACTION_TYPE.FETCH_HORSES_FAILURE:
+    case "FETCH_HORSES_FAILURE":
       return {
         ...state,
         status: "loadingFailure",
@@ -39,16 +39,18 @@ const reducer: Reducer = (state, action) => {
 export const App: FC = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { status, horses, page } = state;
+  const isHorseListPage = page === "horse-list";
+  const isHorsePage = page === "horse";
 
   const handleFetchHorses = () => {
     dispatch({
-      type: ACTION_TYPE.FETCH_HORSES,
+      type: "FETCH_HORSES",
     });
 
     fetchHorses()
       .then((horses: Horse[]) => {
         dispatch({
-          type: ACTION_TYPE.FETCH_HORSES_SUCCESS,
+          type: "FETCH_HORSES_SUCCESS",
           payload: horses,
         });
       })
@@ -56,31 +58,26 @@ export const App: FC = () => {
         console.error(error);
 
         dispatch({
-          type: ACTION_TYPE.FETCH_HORSES_FAILURE,
+          type: "FETCH_HORSES_FAILURE",
         });
       });
   };
 
-  useEffect(() => {
-    handleFetchHorses();
-  }, []);
-
-  useEffect(() => {
-    if (status === 'loadingFailure') {
-      alert('Can not load horses. Please see console message for details');
-    }
-  }, [status])
-
-  const isLoadingHorses = status === 'loading';
-  const isHorseList = page === 'horse-list';
-
   return (
     <main className="App">
-      {isHorseList && <HorseList horses={horses} isLoading={isLoadingHorses} />}
+      {isHorseListPage && (
+        <HorseList
+          horses={horses}
+          status={status}
+          fetchHorses={handleFetchHorses}
+        />
+      )}
     </main>
   );
 };
 
 function fetchHorses() {
-  return fetch(`${process.env.REACT_APP_BASE_URL}/horse`).then((result) => result.json());
+  return fetch(`${process.env.REACT_APP_BASE_URL}/horse`).then((result) =>
+    result.json()
+  );
 }
--- src/HorseList/HorseList.tsx
+++ src/HorseList/HorseList.tsx
@@ -1,47 +1,97 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 
 import { Page } from "../Page";
-import { Button } from "../Button";
+import { Button, TextButton } from "../Button";
+import { ButtonGroup } from "../ButtonGroup";
 
-import { State } from "../types";
+import { useClientPagination } from "../customHooks";
+
+import { State, Status } from "../types";
 
 import "./HorseList.css";
 
-type HorseListProps = {
-  isLoading: boolean;
-  horses: State["horses"];
+const NUMBER_OF_HORSES_ON_PAGE = 10;
+
+const text = {
+  header: "My Horses",
+  previousButton: `Previous ${NUMBER_OF_HORSES_ON_PAGE} horses`,
+  nextButton: `Next ${NUMBER_OF_HORSES_ON_PAGE} horses`,
+  loading: "Loading...",
+  loadingFailure: "Can not load horses. Please see console message for details",
 };
 
-export const HorseList: FC<HorseListProps> = ({ horses }) => {
+type HorseListProps = {
+  status: Status;
+  horses: State["horses"];
+  fetchHorses: VoidFunction;
+};
+
+export const HorseList: FC<HorseListProps> = ({
+  horses,
+  status,
+  fetchHorses,
+}) => {
+  const isLoading = status === "loading";
+  const isLoadingFailure = status === "loadingFailure";
+
+  useEffect(() => {
+    fetchHorses();
+  }, [fetchHorses]);
+
+  useEffect(() => {
+    if (isLoadingFailure) {
+      alert(text.loadingFailure);
+    }
+  }, [isLoadingFailure]);
+
+  const {
+    currentPageData: currentHorses,
+    isFirstPage,
+    isLastPage,
+    nextPage,
+    previousPage,
+  } = useClientPagination(NUMBER_OF_HORSES_ON_PAGE, horses);
+
   return (
-    <Page header="My Horses">
-      <HorseListWrapper>
-        {horses.map(({ id, name }) => (
-          <HorseListItem key={id}>
-            <HorseListItemHeader>
-              <Button className="HorseListButton">{name}</Button>
-            </HorseListItemHeader>
-          </HorseListItem>
-        ))}
-      </HorseListWrapper>
+    <Page header={text.header}>
+      {isLoading && <p>{text.loading}</p>}
+      {!isLoading && (
+        <HorseListWrapper>
+          {currentHorses.map(({ id, name }) => (
+            <HorseListItem key={id}>
+              <HorseListItemHeader>
+                <Button className="HorseListButton">{name}</Button>
+              </HorseListItemHeader>
+            </HorseListItem>
+          ))}
+        </HorseListWrapper>
+      )}
+      <ButtonGroup>
+        <TextButton onClick={previousPage} disabled={isFirstPage}>
+          {text.previousButton}
+        </TextButton>
+        <TextButton onClick={nextPage} disabled={isLastPage}>
+          {text.nextButton}
+        </TextButton>
+      </ButtonGroup>
     </Page>
   );
 };
 
 const HorseListWrapper: FC = ({ children, ...props }) => (
-  <ul className="HorseListWrapper" {...props}>
+  <ul {...props} className="HorseListWrapper">
     {children}
   </ul>
 );
 
 const HorseListItem: FC = ({ children, ...props }) => (
-  <li className="HorseListItem" {...props}>
+  <li {...props} className="HorseListItem" data-testid="horse-list-item">
     {children}
   </li>
 );
 
 const HorseListItemHeader: FC = ({ children, ...props }) => (
-  <h2 className="HorseListItemHeader" {...props}>
+  <h2 {...props} className="HorseListItemHeader">
     {children}
   </h2>
 );
