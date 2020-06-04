8b20173d3f713633ac34df4c790702711423b68c
Anton Serous <thech.ch@gmail.com>
2020-06-05 09:56:50 +1200

    Add button slot in Page component

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -4,13 +4,23 @@ import "./Page.css";
 
 type PageProps = {
   header: ReactNode;
+  headerButton?: ReactNode;
 };
 
-export const Page: FC<PageProps> = ({ children, header }) => (
+export const Page: FC<PageProps> = ({ children, header, headerButton }) => (
   <article className="Page">
-    <Header>{header}</Header>
+    <HeaderWrapper>
+      <Header>{header}</Header>
+      {headerButton}
+    </HeaderWrapper>
     {children}
   </article>
+);
+
+const HeaderWrapper: FC = ({ children }) => (
+  <div className="HeaderWrapper" data-testid="page-header-wrapper">
+    {children}
+  </div>
 );
 
 const Header: FC = ({ children, ...props }) => (
